441a9044ed71584b4abbee4c7b9be8bdd2318b7c
JingYang0519 <jingyang_cell@163.com>
2024-11-20 13:58:28 +0800

    Update fig1_NMF_make.Count_data.celltype.R

--- fig1_NMF_make.Count_data.celltype.R
+++ fig1_NMF_make.Count_data.celltype.R
@@ -4,8 +4,8 @@
 library(Seurat)
 
 print("Loading RDS...")
-out <- "/jdfsbjcas1/ST_BJ/P21Z28400N0234/chenruiying/01.Programs/2023_Embryo/01.Scripts/02.NMF/00.Count_data/"
-rds <- readRDS("/jdfsbjcas1/ST_BJ/P21Z28400N0234/hanrui/At/STO_embryo_scRNA/all_time_merge/allChr_v1/rmR1802/all_sample_subCluster_CellType/At_merge.seurat_clusters_renamed.rds")
+out <- "02.NMF/00.Count_data/"
+rds <- readRDS("At_merge.seurat_clusters_renamed.rds")
 
 if (!file.exists(out)) {
   dir.create(out)
