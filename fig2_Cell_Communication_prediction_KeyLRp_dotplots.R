6e32b63052455851c8c0a73dce2ecf62db58c2e9
JingYang0519 <jingyang_cell@163.com>
2024-11-20 14:21:44 +0800

    Update fig2_Cell_Communication_prediction_KeyLRp_dotplots.R

--- fig2_Cell_Communication_prediction_KeyLRp_dotplots.R
+++ fig2_Cell_Communication_prediction_KeyLRp_dotplots.R
@@ -3,7 +3,7 @@ library(ggplot2)
 library(viridis)
 library(patchwork)
 
-rds <- readRDS("/jdfsbjcas1/ST_BJ/P21Z28400N0234/hanrui/At/STO_embryo_scRNA/all_time_merge/allChr_v1/rmR1802/all_sample_subCluster_CellType/At_merge.seurat_clusters_renamed.rds")
+rds <- readRDS("At_merge.seurat_clusters_renamed.rds")
 
 unique(rds$Organ)
 sub_rds <- rds
@@ -27,6 +27,6 @@ p3 <- DotPlot(object = sub_rds, scale.by = "size", group.by = "CellClass", featu
   scale_y_discrete(position = "right")  + NoLegend() +
     scale_color_viridis(option="rocket", begin = 1, end = 0.6)
 
-pdf("/jdfsbjcas1/ST_BJ/P21Z28400N0234/chenruiying/01.Programs/2023_Embryo/01.Scripts/03.CellComm/02.Results/Comm.SEE.Genes.Ligand.pdf", 30, 10)
+pdf("Comm.SEE.Genes.Ligand.pdf", 30, 10)
 p1 + p3
 dev.off()
