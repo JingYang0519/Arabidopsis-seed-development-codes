43df37f6a6cd9872243664884a0476733bc753f8
JingYang0519 <jingyang_cell@163.com>
2024-11-20 11:40:28 +0800

    Update fig3_CytoTRACE_significance.R

--- fig3_CytoTRACE_significance.R
+++ fig3_CytoTRACE_significance.R
@@ -6,14 +6,14 @@ cells2 <- read.delim("2-28h_Endosperm.cell.Common.genelist.tsv", header = F)
 
 cells2$V1 <- gsub("03_embryo_28h", "02_embryo_28h", cells2$V1)
 
-## Add RALF5RALF29 label -------------------------------------------------------
+## Add typeII label -------------------------------------------------------
 for (i in 1:nrow(cyto)) {
   if (row.names(cyto)[i] %in% c(cells1$V1,cells2$V1)) {
-    cyto$Phenotype[i] <- paste0(cyto$Phenotype[i], "_RALF5RALF29")
+    cyto$Phenotype[i] <- paste0(cyto$Phenotype[i], "_typeII")
   }
 }
 
-cyto <- cyto[which(cyto$Phenotype != "04.28h_C11.1_Endosperm_RALF5RALF29"),]
+cyto <- cyto[which(cyto$Phenotype != "04.28h_C11.1_Endosperm_typeII"),]
 
 ## Plot data -------------------------------------------------------------------
 library(dplyr)
@@ -26,25 +26,25 @@ arrange(cyto.median, desc(CytoTRACE_median))
 
 cyto$Phenotype <- factor(cyto$Phenotype, levels = c("04.28h_C11.1_Endosperm","03.9-11h_C22_Central_cell","01.9-11h_C10.1_Central_cell",
                                                     "02.9-11h_C21_Syndergid_cell","05.28h_C13_Endosperm",
-                                                    "02.9-11h_C21_Syndergid_cell_RALF5RALF29","05.28h_C13_Endosperm_RALF5RALF29"))
+                                                    "02.9-11h_C21_Syndergid_cell_typeII","05.28h_C13_Endosperm_typeII"))
 library(ggplot2)
 p <- ggplot(cyto, aes(x=Phenotype, y=CytoTRACE)) + 
   geom_boxplot() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-pdf("CytoTRACE.RALF5RALF29.pdf",10,8)
+pdf("CytoTRACE.typeII.pdf",10,8)
 p
 dev.off()
 
 ## t.test significance ---------------------------------------------------------
 celltype1 <- "02.9-11h_C21_Syndergid_cell"
-celltype2 <- "02.9-11h_C21_Syndergid_cell_RALF5RALF29"
+celltype2 <- "02.9-11h_C21_Syndergid_cell_typeII"
 paste0(celltype1," (n = ",length(which(cyto$Phenotype == celltype1)),")")
 paste0(celltype2," (n = ",length(which(cyto$Phenotype == celltype2)),")")
 wilcox.test(cyto$CytoTRACE[which(cyto$Phenotype == celltype1)],cyto$CytoTRACE[which(cyto$Phenotype == celltype2)])
 
 celltype1 <- "05.28h_C13_Endosperm"
-celltype2 <- "05.28h_C13_Endosperm_RALF5RALF29"
+celltype2 <- "05.28h_C13_Endosperm_typeII"
 paste0(celltype1," (n = ",length(which(cyto$Phenotype == celltype1)),")")
 paste0(celltype2," (n = ",length(which(cyto$Phenotype == celltype2)),")")
 wilcox.test(cyto$CytoTRACE[which(cyto$Phenotype == celltype1)],cyto$CytoTRACE[which(cyto$Phenotype == celltype2)])
