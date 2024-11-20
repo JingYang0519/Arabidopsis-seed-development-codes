e6427e827124936d5532def646e29add64ea347e
JingYang0519 <jingyang_cell@163.com>
2024-11-20 14:21:28 +0800

    Update fig2_Cell_Communication_prediction.R

--- fig2_Cell_Communication_prediction.R
+++ fig2_Cell_Communication_prediction.R
@@ -9,7 +9,7 @@ args=commandArgs(T)
 
 ### Load Arabidopsis LR pair data
 print("Preparing data...")
-#load("/jdfsbjcas1/ST_BJ/P21Z28400N0234/chenruiying/01.Programs/2023_Embryo/01.Scripts/03.CellComm/00.Data/LR_pair_ath.RDa") # LR pair
+#load("00.Data/LR_pair_ath.RDa") # LR pair
 LR_pair <- read.delim("PlantPhoneDB.AT.revised.QBLRY.tsv", header = T)
 LR_pair <- data.frame(Ligands = LR_pair$Ligands, Receptors = LR_pair$Receptors)
 
@@ -19,7 +19,7 @@ library(Seurat)
 
 ### Perform cell communication
 print("Loading Seurat object...")
-rds <- readRDS("/jdfsbjcas1/ST_BJ/P21Z28400N0234/hanrui/At/STO_embryo_scRNA/all_time_merge/allChr_v1/rmR1802/all_sample_subCluster_CellType/At_merge.seurat_clusters_renamed.rds")
+rds <- readRDS("At_merge.seurat_clusters_renamed.rds")
 
 print("Subsetting Seurat object...")
 sub_rds <- subset(rds, Organ == args[1])
@@ -51,8 +51,8 @@ LRp <- LRscore(sub_rds2@assays$SCT@data,
                method='Average')
 
 print("Saving Ligand-receptor pair info RDS...")
-saveRDS(LRp, paste0("/jdfsbjcas1/ST_BJ/P21Z28400N0234/chenruiying/01.Programs/2023_Embryo/01.Scripts/03.CellComm/02.Results/LRp_CellType.revised/",args[1],".LRp_CellType.0.1.rds"))
+saveRDS(LRp, paste0("LRp_CellType.revised/",args[1],".LRp_CellType.0.1.rds"))
 
 LRp <- LRp[which(LRp$Pvalue<0.05),]
-write.table(LRp, paste0("/jdfsbjcas1/ST_BJ/P21Z28400N0234/chenruiying/01.Programs/2023_Embryo/01.Scripts/03.CellComm/02.Results/LRp_CellType.revised/",args[1],".LRp_CellType.test.0.1.tsv"), quote = F, sep = "\t", row.names = F, col.names = T)
+write.table(LRp, paste0("LRp_CellType.revised/",args[1],".LRp_CellType.test.0.1.tsv"), quote = F, sep = "\t", row.names = F, col.names = T)
 print("Please perform visualization in your disk!")
